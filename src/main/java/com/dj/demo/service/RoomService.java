e64af18eedbdd4bed696a965bdc5fe7b2565f62a
lw <a5327314@163.com>
2020-03-08 12:48:02 +0800

    room接口

--- src/main/java/com/dj/demo/service/RoomService.java
+++ src/main/java/com/dj/demo/service/RoomService.java
@@ -2,6 +2,9 @@ package com.dj.demo.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.dj.demo.pojo.Room;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
 
 /**
  * @ClassName RoomService
@@ -10,4 +13,8 @@ import com.dj.demo.pojo.Room;
  * @Date 2020/3/6 13:09
  **/
 public interface RoomService extends IService<Room> {
+    List<Room> findRoomAll(Room room) throws Exception;
+
+    void update(Room room) throws Exception;
+
 }
