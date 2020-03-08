25214d916588fad10fcd60547c20a7efd735c3e7
lw <a5327314@163.com>
2020-03-08 12:45:53 +0800

    room mapper

--- src/main/java/com/dj/demo/mapper/RoomMapper.java
+++ src/main/java/com/dj/demo/mapper/RoomMapper.java
@@ -2,6 +2,9 @@ package com.dj.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dj.demo.pojo.Room;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
 
 /**
  * @ClassName RoomMapper
@@ -10,4 +13,10 @@ import com.dj.demo.pojo.Room;
  * @Date 2020/3/6 13:17
  **/
 public interface RoomMapper extends BaseMapper<Room> {
+    List<Room> findRoomAll(Room room) throws DataAccessException;
+
+
+
+    void update(Room room) throws DataAccessException;
+
 }
