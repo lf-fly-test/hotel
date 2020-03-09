5e6300464aeee5390c9552082b5d7971f3020552
lw <a5327314@163.com>
2020-03-09 10:47:22 +0800

    添加 修改状态方法

--- src/main/java/com/dj/demo/web/RoomController.java
+++ src/main/java/com/dj/demo/web/RoomController.java
@@ -2,11 +2,13 @@ package com.dj.demo.web;
 
 import com.dj.demo.common.ResultModel;
 import com.dj.demo.pojo.Room;
+import com.dj.demo.pojo.User;
 import com.dj.demo.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,10 +38,23 @@ public class RoomController {
             return new ResultModel().error("服务器异常"+e.getMessage());
         }
     }
-      @RequestMapping("update")
-    public ResultModel update(Room room){
+        @RequestMapping("update")
+        public ResultModel update(Room room, HttpSession session){
         try {
+
+            User user = (User) session.getAttribute("user");
+            room.setUserName(user.getUserName());
             roomService.update(room);
+            return new ResultModel().success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResultModel().error("服务器异常"+e.getMessage());
+        }
+    }
+    @RequestMapping("updateStatus")
+        public ResultModel updateStatus(Room room){
+        try {
+            roomService.updateStatus(room);
             return new ResultModel().success();
         } catch (Exception e) {
             e.printStackTrace();
