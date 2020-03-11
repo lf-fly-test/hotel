9899bbc8591b572ae5f6a13285eb5fa9a8db2839
cch <ch18734823211@163.com>
2020-03-11 15:45:30 +0800

    添加方法

--- src/main/java/com/dj/demo/mapper/UserMapper.java
+++ src/main/java/com/dj/demo/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.dj.demo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dj.demo.pojo.User;
+import org.springframework.dao.DataAccessException;
 
 /**
  * @ClassName UserMapper
@@ -10,4 +11,12 @@ import com.dj.demo.pojo.User;
  * @Date 2020/3/6 13:15
  **/
 public interface UserMapper extends BaseMapper<User> {
+
+    /**
+     * 办理会员
+     * @throws DataAccessException
+     */
+    void applyForMembership(User user) throws DataAccessException;
+
+
 }
